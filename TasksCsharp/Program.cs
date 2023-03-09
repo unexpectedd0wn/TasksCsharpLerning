8df83578accee655ad5721d731992f58192a718d
anna.starling <anna.starling@bbconsult.co.uk>
2023-03-09 21:59:01 +0200

    Add project files.

---
+++ TasksCsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TasksCsharp.CSharp.pro;
+
+namespace TasksCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _S.S1();
+        }
+    }
+}
